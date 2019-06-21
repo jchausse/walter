8b344c0ac3a18b2c48bd47a9a7b95b241b80cf1b
Jeff Chausse <jeff@chausse.org>
2019-06-20 22:59:48 -0400

    Organized folders, set up for building to GitHub Pages.

---
+++ fractal.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/* Require the path module */
+const path = require('path');
+
+/* Require the Fractal module */
+const fractal = module.exports = require('@frctl/fractal').create();
+
+/* Give your project a title. */
+fractal.set('project.title', 'Walter');
+
+/* Tell Fractal where to look for components. */
+fractal.components.set('path', path.join(__dirname, 'src/components'));
+
+/* Tell Fractal where to look for documentation pages. */
+fractal.docs.set('path', path.join(__dirname, 'src/documentation'));
+
+/* Tell the Fractal web preview plugin where to look for static assets. */
+fractal.web.set('static.path', path.join(__dirname, 'src/public'));
+
+/* Set the static HTML build destination */
+fractal.web.set('builder.dest', __dirname + '/docs');
